3c54ab6a4835515d257773f0a957cee0e6d55544
ahmedhosam283843 <ahmed.hosam@coformatique.com>
2022-12-16 12:16:27 +0200

    Retrofit Setup

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/di/AppModule.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/di/AppModule.kt
@@ -1,11 +1,31 @@
 package com.plcoding.jetpackcomposepokedex.di
 
+import com.plcoding.jetpackcomposepokedex.data.remote.PokemonApi
+import com.plcoding.jetpackcomposepokedex.repository.PokemonRepository
+import com.plcoding.jetpackcomposepokedex.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-    
+    @Singleton
+    @Provides
+    fun providePokemonRepo(
+        api: PokemonApi
+    ) = PokemonRepository(api)
+
+    @Singleton
+    @Provides
+    fun providePokemonApi(): PokemonApi {
+        return Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constants.baseUrl)
+            .build()
+            .create(PokemonApi::class.java)
+    }
 }
---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.plcoding.jetpackcomposepokedex.util
+
+object Constants {
+    const val baseUrl  = "https://pokeapi.co/api/v2/"
+}

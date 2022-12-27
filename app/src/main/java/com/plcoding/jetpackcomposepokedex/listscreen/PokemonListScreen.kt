5a619c942b564e17a7c5834a5bc0df9d7031341d
ahmedhosam283843 <ahmed.hosam@coformatique.com>
2022-12-27 18:58:16 +0200

    feat: add search pokemon functionality

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/listscreen/PokemonListScreen.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/listscreen/PokemonListScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.compose.SubcomposeAsyncImage
@@ -43,7 +44,8 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 
 @Composable
 fun PokemonListScreen(
-    navController: NavController
+    navController: NavController,
+    viewModel: PokemonListViewModel = hiltViewModel()
 ) {
     Surface(
         color = MaterialTheme.colors.background,
@@ -63,7 +65,7 @@ fun PokemonListScreen(
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
-
+                viewModel.searchPokemonList(it)
             }
             Spacer(modifier = Modifier.height(16.dp))
             PokemonList(navController = navController)
@@ -101,7 +103,7 @@ fun SearchBar(
                 .background(Color.White, CircleShape)
                 .padding(horizontal = 20.dp, vertical = 12.dp)
                 .onFocusChanged {
-                    isHintDisplayed = !it.isFocused
+                    isHintDisplayed = !it.isFocused && text.isEmpty()
                 }
         )
         if (isHintDisplayed) {
@@ -125,6 +127,8 @@ fun PokemonList(
     val endReached by remember { viewModel.endReached }
     val loadError by remember { viewModel.loadError }
     val isLoading by remember { viewModel.isLoading }
+    var isSearching by remember{viewModel.isSearching}
+
 
     LazyColumn(contentPadding = PaddingValues(16.dp)) {
         val itemCount = if (pokemonList.size % 2 == 0) pokemonList.size / 2
@@ -132,7 +136,7 @@ fun PokemonList(
             pokemonList.size / 2 + 1
         }
         items(itemCount) {
-            if (it >= itemCount - 1 && !endReached && !isLoading) {
+            if (it >= itemCount - 1 && !endReached && !isLoading  && !isSearching) {
                 viewModel.loadPokemonPaginated()
             }
             PokemonRow(rowIndex = it, entries = pokemonList, navController = navController)
--- app/src/main/java/com/plcoding/jetpackcomposepokedex/listscreen/PokemonListViewModel.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/listscreen/PokemonListViewModel.kt
@@ -14,6 +14,7 @@ import com.plcoding.jetpackcomposepokedex.repository.PokemonRepository
 import com.plcoding.jetpackcomposepokedex.util.Constants.PAGE_SIZE
 import com.plcoding.jetpackcomposepokedex.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
@@ -28,10 +29,41 @@ class PokemonListViewModel @Inject constructor(
     var isLoading = mutableStateOf(false)
     var endReached = mutableStateOf(false)
 
+    private var cachedPokemonList = listOf<PokemonListEntry>()
+    private var isSearchStarting = true
+    val isSearching = mutableStateOf(false)
+
+
     init {
         loadPokemonPaginated()
     }
 
+    fun searchPokemonList(query: String) {
+        val listToSearch = if (isSearchStarting) {
+            pokemonList.value
+        } else {
+            cachedPokemonList
+        }
+        viewModelScope.launch (Dispatchers.Default){
+            if (query.isEmpty()){
+                pokemonList.value = cachedPokemonList
+                isSearching.value = false
+                isSearchStarting = true
+                return@launch
+            }
+            val results = listToSearch.filter {
+                it.pokemonName.contains(query.trim(), ignoreCase = true)
+                        || it.number.toString() == query.trim()
+            }
+            if (isSearchStarting){
+                cachedPokemonList = pokemonList.value
+                isSearchStarting = false
+            }
+            pokemonList.value = results
+            isSearching.value = true
+        }
+
+    }
 
     fun loadPokemonPaginated() {
         viewModelScope.launch {
@@ -46,7 +78,8 @@ class PokemonListViewModel @Inject constructor(
                         } else {
                             entry.url.takeLastWhile { it.isDigit() }
                         }
-                        val imageUrl  = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
+                        val imageUrl =
+                            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
                         PokemonListEntry(entry.name.replaceFirstChar {
                             if (it.isLowerCase()) it.titlecase(
                                 Locale.ROOT
@@ -54,9 +87,9 @@ class PokemonListViewModel @Inject constructor(
                         }, imageUrl, number.toInt())
                     }
                     currentPage++
-                    loadError.value =""
+                    loadError.value = ""
                     isLoading.value = false
-                    pokemonList.value+= pokemonEntries
+                    pokemonList.value += pokemonEntries
                 }
                 is Resource.Error -> {
                     loadError.value = result.message!!
